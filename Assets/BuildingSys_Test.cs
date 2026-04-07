b8d7a3e5d318111c184cc4c4745923ce11cde0ce
Maxtukly <maxym.ulyan@gmail.com>
2026-04-07 10:16:12 +0300

    Grid created + demo building
    3 Grids added: main, background, UI
    Basic building system added

---
+++ Assets/BuildingSys_Test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildingSys_Test : MonoBehaviour
+{
+    [SerializeField]Tile tile;
+    [SerializeField]Tilemap grid;
+    Vector3 mousepos;
+    Vector3Int cellpos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cellpos = grid.WorldToCell(mousepos);
+        if (Input.GetMouseButtonDown(0) && !grid.HasTile(cellpos))
+        {
+            grid.SetTile(cellpos, tile);
+        }
+    }
+}
