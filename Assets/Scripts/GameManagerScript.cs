f01184592fa82a773a484be4192dc52c275c61b8
Dark1n-s <thedarkin031@gmail.com>
2026-04-15 18:59:23 +0300

    UI update

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,6 +26,11 @@ public class GameManager : MonoBehaviour
 
     [Tooltip("Unused Energy")]
     public float UnusedEnergy = 0f;
+
+    [Header("UI")]
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI energyText;
 
 
 
@@ -52,6 +58,29 @@ public class GameManager : MonoBehaviour
             currentTime -= 24f;
         }
         DistributeEnergy();
+        if (GameManager.Instance == null) return;
+        UpdateClockUI();
+        UpdateEconomicsUI();
+    }
+
+
+    private void UpdateClockUI()
+    {
+
+        float rawTime = GameManager.Instance.currentTime;
+
+        int hours = Mathf.FloorToInt(rawTime);
+
+        int minutes = Mathf.FloorToInt((rawTime - hours) * 60f);
+
+        timeText.text = $"Час: {hours:00}:{minutes:00}";
+    }
+
+    private void UpdateEconomicsUI()
+    {
+        moneyText.text = $"Бюджет: {GameManager.Instance.moneyBalance:F0} $";
+
+        energyText.text = $"Залишок енергії: {GameManager.Instance.UnusedEnergy:F1} кВт";
     }
 
     public void DistributeEnergy()
